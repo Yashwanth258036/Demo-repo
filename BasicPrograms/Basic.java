0e2328d9396ad784265fc15b9613664d8fb0c513
Yashwanth258036 <yashwanthsai898@gmail.com>
2025-09-22 10:50:38 +0530

    BasicJava

---
+++ BasicPrograms/Basic.java
@@ -0,0 +1,19 @@
+package BasicPrograms;
+
+public class Basic {
+    public static void main(String[] args)
+     {
+        int x = 12; 
+        int y = 12; 
+        System.out.println("The sum is :"+x+y); //1212 
+        System.out.println(+x+y);//24 
+        System.out.println(""+x+y);//1212 
+        System.out.println("The sum is :"+(x+y));//24 
+        String str = 90 + 12 + "NIT" + (40 + 40 + 40) ; 
+         System.out.println(str); 
+        
+        System.out.println(Integer.parseInt("898"));
+        
+    }
+    
+}
